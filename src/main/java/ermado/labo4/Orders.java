dea8e7d7e6adaa45f55232bc241212fe55ad3d23
Marion Dutu <marion.dutulaunay@heig-vd.ch>
2019-05-17 10:49:38 +0200

    Méthode : Combine Functions into Class, Classes OrdersWriter et Orders, Bad Smell : Object-Orientation Abusers

--- src/main/java/ermado/labo4/Orders.java
+++ src/main/java/ermado/labo4/Orders.java
@@ -17,4 +17,65 @@ public class Orders {
     public Order getOrder(int i) {
         return orders.get(i);
     }
+
+    public StringBuffer getOrdersToString() {
+
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < this.getOrdersCount(); i++) {
+            Order order = this.getOrder(i);
+            sb.append("{");
+            sb.append("\"id\": ");
+            sb.append(order.getOrderId());
+            sb.append(", ");
+            sb.append("\"products\": [");
+
+            sb.append(getProductsToString(order));
+
+            if (order.getProductsCount() > 0) {
+                sb.delete(sb.length() - 2, sb.length());
+            }
+
+            sb.append("]");
+            sb.append("}, ");
+        }
+
+        if (this.getOrdersCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        return sb;
+    }
+
+    public StringBuffer getProductsToString(Order order) {
+
+        StringBuffer sb = new StringBuffer();
+
+        for (int j = 0; j < order.getProductsCount(); j++) {
+            Product product = order.getProduct(j);
+
+            sb.append("{");
+            sb.append("\"code\": \"");
+            sb.append(product.getCode());
+            sb.append("\", ");
+            sb.append("\"color\": \"");
+            sb.append(product.getColorFor());
+            sb.append("\", ");
+
+            if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
+                sb.append("\"size\": \"");
+                sb.append(product.getSizeFor());
+                sb.append("\", ");
+            }
+
+            sb.append("\"price\": ");
+            sb.append(product.getPrice());
+            sb.append(", ");
+            sb.append("\"currency\": \"");
+            sb.append(product.getCurrency());
+            sb.append("\"}, ");
+        }
+
+        return sb;
+    }
 }
