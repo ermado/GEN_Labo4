803615c572fc04065387505160dc4e9f10caa8c1
Marion Dutu <marion.dutulaunay@heig-vd.ch>
2019-05-17 10:41:15 +0200

    Méthode : Extract Method, Classe OrdersWriter, Bad Smell : Bloaters

--- src/main/java/ermado/labo4/OrdersWriter.java
+++ src/main/java/ermado/labo4/OrdersWriter.java
@@ -27,30 +27,7 @@ public class OrdersWriter {
             sb.append(", ");
             sb.append("\"products\": [");
 
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-
-                sb.append("{");
-                sb.append("\"code\": \"");
-                sb.append(product.getCode());
-                sb.append("\", ");
-                sb.append("\"color\": \"");
-                sb.append(product.getColorFor());
-                sb.append("\", ");
-
-                if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
-                    sb.append("\"size\": \"");
-                    sb.append(product.getSizeFor());
-                    sb.append("\", ");
-                }
-
-                sb.append("\"price\": ");
-                sb.append(product.getPrice());
-                sb.append(", ");
-                sb.append("\"currency\": \"");
-                sb.append(product.getCurrency());
-                sb.append("\"}, ");
-            }
+            sb.append(getProductsToString(order));
 
             if (order.getProductsCount() > 0) {
                 sb.delete(sb.length() - 2, sb.length());
@@ -62,6 +39,38 @@ public class OrdersWriter {
 
         if (orders.getOrdersCount() > 0) {
             sb.delete(sb.length() - 2, sb.length());
+        }
+
+        return sb;
+    }
+
+    public StringBuffer getProductsToString(Order order) {
+
+        StringBuffer sb = new StringBuffer();
+
+        for (int j = 0; j < order.getProductsCount(); j++) {
+            Product product = order.getProduct(j);
+
+            sb.append("{");
+            sb.append("\"code\": \"");
+            sb.append(product.getCode());
+            sb.append("\", ");
+            sb.append("\"color\": \"");
+            sb.append(product.getColorFor());
+            sb.append("\", ");
+
+            if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
+                sb.append("\"size\": \"");
+                sb.append(product.getSizeFor());
+                sb.append("\", ");
+            }
+
+            sb.append("\"price\": ");
+            sb.append(product.getPrice());
+            sb.append(", ");
+            sb.append("\"currency\": \"");
+            sb.append(product.getCurrency());
+            sb.append("\"}, ");
         }
 
         return sb;
